35734121e281f32850cc0610355c066b1da5acff
Sahil B. Rewari <seobythebay@gmail.com>
2025-04-19 14:10:24 +0530

    Add files via upload

---
+++ app/code/SEOByTheBay/RemoveKeywords/Plugin/PageConfig/RemoveKeywords.php
@@ -0,0 +1,9 @@
+<?php namespace SEOByTheBay\RemoveKeywords\Plugin\PageConfig;
+
+class RemoveKeywords
+{
+    public function afterGetKeywords($subject, $return)
+    { 
+        return '';
+    }
+}
